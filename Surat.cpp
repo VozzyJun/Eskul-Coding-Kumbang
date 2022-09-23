2cde2409008d94b1a77dc0b32164614c98a273b1
VozzyJun <vincentjunio1@gmail.com>
2022-09-23 17:19:51 +0700

    Message from Runfig

---
+++ Surat.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	cout<<"|  Dear \n";
+	cout<<"|  Vinny \n";
+	cout<<"|  'Prismaltopia's Message System' Vindicado Vindication\n\n";
+	
+	cout<<"If you got this message ... \n";
+	cout<<"Then i'm dead ... \n";
+	cout<<"But hear me out ...! \n";
+	cout<<"There's something i need to tell you about her. The real her \n\n\n";
+	cout<<"It's ... \n\n";
+	cout<<"oh god- its breaking. Argh-";
+	cout<<"01010011 01110100 01100001 01111001 00100000 01100001 01110111 01100001 01111001 00100000 01100110 01110010 01101111 01101101 00100000 01001111 01110010 01110000 01101000 01101001 01100001 00100001 \n"; 
+	cout<<" \n\n\n\n\n\n\n\n";
+	cout<<" -Runfig\n";
+	
+	
+	
+	
+	getch();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
